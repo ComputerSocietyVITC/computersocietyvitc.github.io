03637b96a8e7b697ba89c3dc15dc7a1a3ca4bd29
Samank Gupta <samankgupta@gmail.com>
2021-08-25 22:59:45 +0530

    Contact Us Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'comsocgreen': '#29DB9B',
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
