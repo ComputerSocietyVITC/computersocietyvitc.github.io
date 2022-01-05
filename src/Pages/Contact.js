518873dec3f7a1895d380c1c585bb509afb3064f
TejasV58 <tejasv515@gmail.com>
2022-01-05 23:23:00 +0530

    images added

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,14 +3,18 @@ import instagram from '../images/socialmedia/instagram.png'
 import github from '../images/socialmedia/github.png'
 import linkedin from '../images/socialmedia/linkedin.png'
 import discord from '../images/socialmedia/discord.png'
+import asteroid from "../images/asteroid.png";
 import Mountains from "../components/Mountains";
 
 const Contact = () => {
     return (
         <div>
+            <div className="font-catamaran text-center my-5 text-black" id="outlinetext">
+                <h1>CONTACT US</h1>
+            </div>
             <div className="grid grid-cols-2 gap-2">
-                <div className="pb-16 mt-20">
-                    <h2 className="text-5xl text-left sm:ml-40 mt-12"><span className="text-comsocgreen">Contact</span> Us</h2>
+                <div className="pb-16 mt-10">
+                    
                     <div>
                         <div className="sm:ml-40 mt-6 sm:mr-24">
                             <div className="mt-5 md:mt-0 md:col-span-2 text-left">
@@ -18,7 +22,7 @@ const Contact = () => {
                                     <div className="sm:rounded-md">
 
                                         <div className="col-span-6">
-                                            <label htmlFor="name" className="block text-md font-medium text-gray-700 dark:text-gray-200">
+                                            <label htmlFor="name" className="block text-md font-medium text-white ">
                                                 Name
                                             </label>
                                             <input
@@ -26,11 +30,11 @@ const Contact = () => {
                                                 name="name"
                                                 id="name"
                                                 autoComplete="name"
-                                                className="mt-1 focus:ring-comsocgreen focus:border-comsocgreen block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                                className="mt-1 focus:ring-comsocgreen focus:border-comsocgreen block w-full shadow-sm sm:text-sm border-gray-300 rounded-md "
                                             />
                                         </div><br />
                                         <div className="col-span-6">
-                                            <label htmlFor="email" className="block text-md font-medium text-gray-700 dark:text-gray-200">
+                                            <label htmlFor="email" className="block text-md font-medium text-white">
                                                 Email ID
                                             </label>
                                             <input
@@ -42,7 +46,7 @@ const Contact = () => {
                                             />
                                         </div><br />
                                         <div className="col-span-6">
-                                            <label htmlFor="subject" className="block text-md font-medium text-gray-700 dark:text-gray-200">
+                                            <label htmlFor="subject" className="block text-md font-medium text-white">
                                                 Subject
                                             </label>
                                             <input
@@ -55,7 +59,7 @@ const Contact = () => {
                                         </div><br />
                                         <div className="space-y-6">
                                             <div>
-                                                <label htmlFor="message" className="block text-md font-medium text-gray-700 dark:text-gray-200">
+                                                <label htmlFor="message" className="block text-md font-medium text-white">
                                                     Message
                                                 </label>
                                                 <div className="mt-1">
@@ -84,18 +88,8 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <div className="sm:mr-40 sm:mt-60 sm:ml-24">
-                    <h2 className="text-lg font-medium text-left mb-4">Address</h2>
-                    <p className="text-lg text-left">Kelambakkam - Vandalur Rd, Rajan Nagar, Chennai, Tamil Nadu 600127</p><br />
-                    <h2 className="text-lg font-medium text-left mb-4">Email ID</h2>
-                    <a href="mailto:ieeecomputersociety@vit.ac.in"><p className="text-lg text-left">ieeecomputersociety@vit.ac.in</p></a><br />
-                    <h2 className="text-lg font-medium text-left mb-4">Social Media</h2>
-                    <div className="w-1/2 grid grid-cols-4 gap-4">
-                        <a href="https://www.instagram.com/comsoc.vitcc/" target="_blank"><img src={instagram} alt="Instagram" className="h-11 col-span-1"></img></a>
-                        <a href="https://github.com/ComputerSocietyVITC" target="_blank"><img src={github} alt="Github" className="h-11 col-span-1"></img></a>
-                        <a href="https://www.linkedin.com/company/ieee-computer-society-vit-chennai/" target="_blank"><img src={linkedin} alt="LinkedIn" className="h-11 col-span-1"></img></a>
-                        <a href="https://discord.gg/6vkY3kcZnE" target="_blank"><img src={discord} alt="Discord" className="h-11 col-span-1"></img></a>
-                    </div>
+                <div className='relative'>
+                   <img src={asteroid} alt="asteroid" style={{top:"-100px"}} />
                 </div>
             </div>
             <Mountains />
