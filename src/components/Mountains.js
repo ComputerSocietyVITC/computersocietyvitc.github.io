02f8336c8bc0b0c4747775ee1295f4de535e4ca9
Samank Gupta <samankgupta@gmail.com>
2022-01-08 16:45:20 +0530

    homepage design changes

--- src/components/Mountains.js
+++ src/components/Mountains.js
@@ -7,7 +7,7 @@ const Mountains = () => {
         <div className="grid grid-cols-1 lg:grid-cols-2 relative pt-12">
             {window.location.href.toLowerCase().split('/').slice(-1)[0] === '' ?
                 <div className="w-full absolute bottom-0 opacity-0 hover:opacity-100 delay-500 duration-1000 z-10" style={{ left: "48.5%" }}>
-                    <img src={dark} alt="dark" className="w-10" />
+                    <img src={dark} alt="dark" className="w-8 md:w-10" />
                 </div>
                 : <></>}
             <div>
