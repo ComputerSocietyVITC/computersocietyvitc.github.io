09b7a2de441bef707afe35cf4429571425c9fd2a
TejasV58 <tejasv515@gmail.com>
2022-01-11 23:50:57 +0530

    scrollbar style done

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -61,14 +61,9 @@ const Blogs = () => {
         loading ? 
         (<Loader/>) :
         (<div className='relative z-10'>
-          <div className="font-catamaran text-center mt-10 mb-2 lg:text-left lg:ml-28 text-black" id="outlinetext">
+          <div className="font-catamaran text-center mt-10 mb-2 lg:text-center text-black" id="outlinetext">
             <h1>OUR BLOGS</h1>
           </div>
-          <div className="m-auto mb-12 lg:mb-16 flex">
-            <div className="w-4/5 m-auto lg:mt-20 lg:ml-32 lg:w-3/5"><p className="text-center lg:text-left text-xl lg:text-2xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis alias nemo, corrupti recusandae pariatur dicta quam culpa ad, fugiat quisquam harum? Animi ex natus repellendus beatae totam. Placeat magnam delectus, iste architecto perspiciatis maiores iure totam tempore aperiam corrupti quos temporibus error expedita optio molestiae corporis possimus rem doloremque voluptatibus.</p></div>
-            <div className="hidden lg:block m-auto -mt-32 -mr-10 content-center overflow-hidden"><img className="object-cover" src={blog} alt="" /></div>
-          </div>
-          <p className="font-bold font-sans text-3xl">Check Out Our Blogs</p>
           <div className="w-2/3 m-auto lg:w-full mt-16 lg:px-28 grid grid-cols-1 lg:grid-cols-3 grid-flow-cols gap-10 lg:gap-16">
             {
               articles.map((article) => (<Article author={article.author} date={article.publishedDate} title={article.title} url={article.url} img={article.imgSrc} />))
