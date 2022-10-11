3bc21a425b1fa32c6b7adc8c6a2a0b85a6ed26bc
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-10-11 20:23:15 +0530

    Members List bug patch
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/components/bookdata/MembersList.js
+++ src/components/bookdata/MembersList.js
@@ -1,498 +1,504 @@
 const MembersList = [
   {
-    "Members":
-      [{
-        "Name": "A Rahul",
-        "Reg_No": "20BRS1102"
+    Members: [
+      {
+        Name: "A Rahul",
+        Reg_No: "20BRS1102",
       },
       {
-        "Name": "Aadhithya D",
-        "Reg_No": "21BPS1236"
+        Name: "Aadhithya D",
+        Reg_No: "21BPS1236",
       },
       {
-        "Name": "Aarush Sinha",
-        "Reg_No": "21BPS1142"
+        Name: "Aarush Sinha",
+        Reg_No: "21BPS1142",
       },
       {
-        "Name": "Aaryan Mehta",
-        "Reg_No": "20BAI1108"
+        Name: "Aaryan Mehta",
+        Reg_No: "20BAI1108",
       },
       {
-        "Name": "Abhijith Ganesh",
-        "Reg_No": "21BRS1200"
+        Name: "Abhijith Ganesh",
+        Reg_No: "21BRS1200",
       },
       {
-        "Name": "Aditya Narayan Mahapatra",
-        "Reg_No": "21BRS1114"
+        Name: "Aditya Narayan Mahapatra",
+        Reg_No: "21BRS1114",
       },
       {
-        "Name": "Aditya Sai Sd",
-        "Reg_No": "21BCE1889"
+        Name: "Aditya Sai Sd",
+        Reg_No: "21BCE1889",
       },
       {
-        "Name": "Alan K Alex",
-        "Reg_No": "20BAI1226"
+        Name: "Alan K Alex",
+        Reg_No: "20BAI1226",
       },
       {
-        "Name": "Amudhini P K",
-        "Reg_No": "19BCE1492"
+        Name: "Amudhini P K",
+        Reg_No: "19BCE1492",
       },
       {
-        "Name": "Anand Sai Kumar",
-        "Reg_No": "21BEE1152"
+        Name: "Anand Sai Kumar",
+        Reg_No: "21BEE1152",
       },
       {
-        "Name": "Ananth Eswar",
-        "Reg_No": "21BAI1263"
+        Name: "Ananth Eswar",
+        Reg_No: "21BAI1263",
       },
       {
-        "Name": "Ananya Chenat",
-        "Reg_No": "21BCE1027"
+        Name: "Ananya Chenat",
+        Reg_No: "21BCE1027",
       },
       {
-        "Name": "Anika Kamath",
-        "Reg_No": "21BRS1296"
+        Name: "Anika Kamath",
+        Reg_No: "21BRS1296",
       },
       {
-        "Name": "Anisha Kang",
-        "Reg_No": "20BAI1160"
+        Name: "Anisha Kang",
+        Reg_No: "20BAI1160",
       },
       {
-        "Name": "Anjali Kedia",
-        "Reg_No": "21BAI1130"
-      }]
+        Name: "Anjali Kedia",
+        Reg_No: "21BAI1130",
+      },
+    ],
   },
   {
-    "Members":
-      [,
-        {
-          "Name": "Anugrah Toby",
-          "Reg_No": "21BCE1510"
-        },
-        {
-          "Name": "Apoorv D",
-          "Reg_No": "21BCE1169"
-        },
-        {
-          "Name": "Aran Agarwal",
-          "Reg_No": "21BCE5443"
-        },
-        {
-          "Name": "Aravind Srinivas S",
-          "Reg_No": "21BCE1733"
-        },
-        {
-          "Name": "Aritro Saha",
-          "Reg_No": "21BLC1174"
-        },
-        {
-          "Name": "Arnav Sharma",
-          "Reg_No": "21BCE6066"
-        },
-        {
-          "Name": "Aryan Vigyat",
-          "Reg_No": "20BCE1452"
-        },
-        {
-          "Name": "Ashish Ram J A",
-          "Reg_No": "21BLC1538"
-        },
-        {
-          "Name": "Avadhanam Karthikeya",
-          "Reg_No": "20BEC1059"
-        },
-        {
-          "Name": "Avikal Gupta",
-          "Reg_No": "21BPS1024"
-        },
-        {
-          "Name": "Ayush Ranjan Roy",
-          "Reg_No": "19BEC1007"
-        },
-        {
-          "Name": "B Santhanakrishnan",
-          "Reg_No": "20BCE1474"
-        },
-        {
-          "Name": "Bhavya Chanana",
-          "Reg_No": "21BCE1414"
-        },
-        {
-          "Name": "Chaitanya Bhude",
-          "Reg_No": "21BAI1445 "
-        },
-        {
-          "Name": "Chirag Gupta",
-          "Reg_No": "21BAI1079"
-        }]
+    Members: [
+      {
+        Name: "Anugrah Toby",
+        Reg_No: "21BCE1510",
+      },
+      {
+        Name: "Apoorv D",
+        Reg_No: "21BCE1169",
+      },
+      {
+        Name: "Aran Agarwal",
+        Reg_No: "21BCE5443",
+      },
+      {
+        Name: "Aravind Srinivas S",
+        Reg_No: "21BCE1733",
+      },
+      {
+        Name: "Aritro Saha",
+        Reg_No: "21BLC1174",
+      },
+      {
+        Name: "Arnav Sharma",
+        Reg_No: "21BCE6066",
+      },
+      {
+        Name: "Aryan Vigyat",
+        Reg_No: "20BCE1452",
+      },
+      {
+        Name: "Ashish Ram J A",
+        Reg_No: "21BLC1538",
+      },
+      {
+        Name: "Avadhanam Karthikeya",
+        Reg_No: "20BEC1059",
+      },
+      {
+        Name: "Avikal Gupta",
+        Reg_No: "21BPS1024",
+      },
+      {
+        Name: "Ayush Ranjan Roy",
+        Reg_No: "19BEC1007",
+      },
+      {
+        Name: "B Santhanakrishnan",
+        Reg_No: "20BCE1474",
+      },
+      {
+        Name: "Bhavya Chanana",
+        Reg_No: "21BCE1414",
+      },
+      {
+        Name: "Chaitanya Bhude",
+        Reg_No: "21BAI1445 ",
+      },
+      {
+        Name: "Chirag Gupta",
+        Reg_No: "21BAI1079",
+      },
+    ],
   },
   {
-    "Members":
-      [
-        {
-          "Name": "Christo Joby Antony",
-          "Reg_No": "21BCE1635"
-        },
-        {
-          "Name": "Cp Gautham Krishna",
-          "Reg_No": "21BRS1032"
-        },
-        {
-          "Name": "Darshini. R",
-          "Reg_No": "20BCE1054"
-        },
-        {
-          "Name": "Deepavarshini T",
-          "Reg_No": "20BCE1661"
-        },
-        {
-          "Name": "Deepthi Balasubramanian",
-          "Reg_No": "21BCE5601"
-        },
-        {
-          "Name": "Devansh Vikram",
-          "Reg_No": "21BCE1554"
-        },
-        {
-          "Name": "Devika Santosh",
-          "Reg_No": "20BAI1099"
-        },
-        {
-          "Name": "Dhanush B",
-          "Reg_No": "20BCE1589"
-        },
-        {
-          "Name": "Dhanush S",
-          "Reg_No": "20BCE1811"
-        },
-        {
-          "Name": "Dikshita Mehta",
-          "Reg_No": "20BCE1056"
-        },
-        {
-          "Name": "Divyansh Chawla",
-          "Reg_No": "21BRS1180"
-        },
-        {
-          "Name": "Divyashree S",
-          "Reg_No": "19BCE1689"
-        },
-        {
-          "Name": "Diya Basu",
-          "Reg_No": "21BPS1159"
-        },
-        {
-          "Name": "Faizah Kureshi",
-          "Reg_No": "20BAI1041"
-        },
-        {
-          "Name": "Guddeti Aryan",
-          "Reg_No": "20BCE1590"
-        }]
+    Members: [
+      {
+        Name: "Christo Joby Antony",
+        Reg_No: "21BCE1635",
+      },
+      {
+        Name: "Cp Gautham Krishna",
+        Reg_No: "21BRS1032",
+      },
+      {
+        Name: "Darshini. R",
+        Reg_No: "20BCE1054",
+      },
+      {
+        Name: "Deepavarshini T",
+        Reg_No: "20BCE1661",
+      },
+      {
+        Name: "Deepthi Balasubramanian",
+        Reg_No: "21BCE5601",
+      },
+      {
+        Name: "Devansh Vikram",
+        Reg_No: "21BCE1554",
+      },
+      {
+        Name: "Devika Santosh",
+        Reg_No: "20BAI1099",
+      },
+      {
+        Name: "Dhanush B",
+        Reg_No: "20BCE1589",
+      },
+      {
+        Name: "Dhanush S",
+        Reg_No: "20BCE1811",
+      },
+      {
+        Name: "Dikshita Mehta",
+        Reg_No: "20BCE1056",
+      },
+      {
+        Name: "Divyansh Chawla",
+        Reg_No: "21BRS1180",
+      },
+      {
+        Name: "Divyashree S",
+        Reg_No: "19BCE1689",
+      },
+      {
+        Name: "Diya Basu",
+        Reg_No: "21BPS1159",
+      },
+      {
+        Name: "Faizah Kureshi",
+        Reg_No: "20BAI1041",
+      },
+      {
+        Name: "Guddeti Aryan",
+        Reg_No: "20BCE1590",
+      },
+    ],
   },
   {
-    "Members":
-      [{
-        "Name": "Harsh Aryan",
-        "Reg_No": "20BCE1850"
+    Members: [
+      {
+        Name: "Harsh Aryan",
+        Reg_No: "20BCE1850",
       },
       {
-        "Name": "Harsh Patil",
-        "Reg_No": "21BCE1030"
+        Name: "Harsh Patil",
+        Reg_No: "21BCE1030",
       },
       {
-        "Name": "Ijas Siju",
-        "Reg_No": "20BCE1580"
+        Name: "Ijas Siju",
+        Reg_No: "20BCE1580",
       },
       {
-        "Name": "Ishaan Iyer",
-        "Reg_No": "20BCE1634"
+        Name: "Ishaan Iyer",
+        Reg_No: "20BCE1634",
       },
       {
-        "Name": "Ishita Agarwal",
-        "Reg_No": "21BCE5317"
+        Name: "Ishita Agarwal",
+        Reg_No: "21BCE5317",
       },
       {
-        "Name": "K S Vikash",
-        "Reg_No": "20BEC1019"
+        Name: "K S Vikash",
+        Reg_No: "20BEC1019",
       },
       {
-        "Name": "K.P.S.Shivratna",
-        "Reg_No": "19BLC1012"
+        Name: "K.P.S.Shivratna",
+        Reg_No: "19BLC1012",
       },
       {
-        "Name": "Karthik G",
-        "Reg_No": "20BCE1021"
+        Name: "Karthik G",
+        Reg_No: "20BCE1021",
       },
       {
-        "Name": "Kaushal Barhate",
-        "Reg_No": "20BCE1099"
+        Name: "Kaushal Barhate",
+        Reg_No: "20BCE1099",
       },
       {
-        "Name": "Kriti Agarwal",
-        "Reg_No": "21BRS1285"
+        Name: "Kriti Agarwal",
+        Reg_No: "21BRS1285",
       },
       {
-        "Name": "Kriti Walia",
-        "Reg_No": "21BRS1125"
+        Name: "Kriti Walia",
+        Reg_No: "21BRS1125",
       },
       {
-        "Name": "Kruthik Ballari",
-        "Reg_No": "21BCE1561"
+        Name: "Kruthik Ballari",
+        Reg_No: "21BCE1561",
       },
       {
-        "Name": "Kushagra Goyal",
-        "Reg_No": "21BCE1157"
+        Name: "Kushagra Goyal",
+        Reg_No: "21BCE1157",
       },
       {
-        "Name": "M V Kaushik",
-        "Reg_No": "20BEC1095"
+        Name: "M V Kaushik",
+        Reg_No: "20BEC1095",
       },
       {
-        "Name": "Manav Garg",
-        "Reg_No": "21BAI1045"
-      }]
+        Name: "Manav Garg",
+        Reg_No: "21BAI1045",
+      },
+    ],
   },
   {
-    "Members":
-      [{
-        "Name": "Manoj Kumar Bhargava",
-        "Reg_No": "21BAI1040"
+    Members: [
+      {
+        Name: "Manoj Kumar Bhargava",
+        Reg_No: "21BAI1040",
       },
       {
-        "Name": "Manoj S K",
-        "Reg_No": "21BAI1010"
+        Name: "Manoj S K",
+        Reg_No: "21BAI1010",
       },
       {
-        "Name": "Mayank Gupta",
-        "Reg_No": "20BCE1538"
+        Name: "Mayank Gupta",
+        Reg_No: "20BCE1538",
       },
       {
-        "Name": "Mohit Varma",
-        "Reg_No": "21BLC1125"
+        Name: "Mohit Varma",
+        Reg_No: "21BLC1125",
       },
       {
-        "Name": "Narayan Subramanian",
-        "Reg_No": "20BAI1207"
+        Name: "Narayan Subramanian",
+        Reg_No: "20BAI1207",
       },
       {
-        "Name": "Neeraj J Manurkar",
-        "Reg_No": "20BRS1269"
+        Name: "Neeraj J Manurkar",
+        Reg_No: "20BRS1269",
       },
       {
-        "Name": "Nikhil Inturi",
-        "Reg_No": "19BCE1419"
+        Name: "Nikhil Inturi",
+        Reg_No: "19BCE1419",
       },
       {
-        "Name": "Nirupama Rathinavel",
-        "Reg_No": "20BRS1060"
+        Name: "Nirupama Rathinavel",
+        Reg_No: "20BRS1060",
       },
       {
-        "Name": "Nishant Kumar",
-        "Reg_No": "20BCE1604"
+        Name: "Nishant Kumar",
+        Reg_No: "20BCE1604",
       },
       {
-        "Name": "Nishanth G",
-        "Reg_No": "20BEE1049"
+        Name: "Nishanth G",
+        Reg_No: "20BEE1049",
       },
       {
-        "Name": "Nitish Menon",
-        "Reg_No": "20BRS1022"
+        Name: "Nitish Menon",
+        Reg_No: "20BRS1022",
       },
       {
-        "Name": "Omar Ashfaq",
-        "Reg_No": "21BRS1015"
+        Name: "Omar Ashfaq",
+        Reg_No: "21BRS1015",
       },
       {
-        "Name": "Onkar Apte",
-        "Reg_No": "20BCE1341"
+        Name: "Onkar Apte",
+        Reg_No: "20BCE1341",
       },
       {
-        "Name": "Prabhat Manoj Singh",
-        "Reg_No": "19BLC1027"
+        Name: "Prabhat Manoj Singh",
+        Reg_No: "19BLC1027",
       },
       {
-        "Name": "Pragyan Sharma",
-        "Reg_No": "21BAI1486"
-      }]
+        Name: "Pragyan Sharma",
+        Reg_No: "21BAI1486",
+      },
+    ],
   },
   {
-    "Members":
-      [{
-        "Name": "Pranav Gupta",
-        "Reg_No": "21BAI1090"
+    Members: [
+      {
+        Name: "Pranav Gupta",
+        Reg_No: "21BAI1090",
       },
       {
-        "Name": "Pranav Induchudan",
-        "Reg_No": "21BCE1778"
+        Name: "Pranav Induchudan",
+        Reg_No: "21BCE1778",
       },
       {
-        "Name": "Pranaydeep Mayank",
-        "Reg_No": "19bai1026"
+        Name: "Pranaydeep Mayank",
+        Reg_No: "19bai1026",
       },
       {
-        "Name": "Praneeth Sethumadhavan",
-        "Reg_No": "19BCE1599"
+        Name: "Praneeth Sethumadhavan",
+        Reg_No: "19BCE1599",
       },
       {
-        "Name": "Prateek Mohanty",
-        "Reg_No": "20BCE1482"
+        Name: "Prateek Mohanty",
+        Reg_No: "20BCE1482",
       },
       {
-        "Name": "Rithvik G",
-        "Reg_No": "21BAI1216"
+        Name: "Rithvik G",
+        Reg_No: "21BAI1216",
       },
       {
-        "Name": "Ritika Kumari Pandey",
-        "Reg_No": "20BCE1598"
+        Name: "Ritika Kumari Pandey",
+        Reg_No: "20BCE1598",
       },
       {
-        "Name": "Rohan Alroy.B",
-        "Reg_No": "20BAI1245"
+        Name: "Rohan Alroy.B",
+        Reg_No: "20BAI1245",
       },
       {
-        "Name": "S. Shruthi",
-        "Reg_No": "20BCE1986"
+        Name: "S. Shruthi",
+        Reg_No: "20BCE1986",
       },
       {
-        "Name": "Sai Sriraj M V H",
-        "Reg_No": "20BAI1224"
+        Name: "Sai Sriraj M V H",
+        Reg_No: "20BAI1224",
       },
       {
-        "Name": "Saimirra R",
-        "Reg_No": "21BCE1656"
+        Name: "Saimirra R",
+        Reg_No: "21BCE1656",
       },
       {
-        "Name": "Saketh",
-        "Reg_No": "21BRS1081"
+        Name: "Saketh",
+        Reg_No: "21BRS1081",
       },
       {
-        "Name": "Sam Tholath Robin",
-        "Reg_No": "21BRS1445"
+        Name: "Sam Tholath Robin",
+        Reg_No: "21BRS1445",
       },
       {
-        "Name": "Sanjay R R",
-        "Reg_No": "20BCE1537"
+        Name: "Sanjay R R",
+        Reg_No: "20BCE1537",
       },
       {
-        "Name": "Sanskriti Sanjay Kumar Singh",
-        "Reg_No": "20BCE1415"
-      }]
+        Name: "Sanskriti Sanjay Kumar Singh",
+        Reg_No: "20BCE1415",
+      },
+    ],
   },
   {
-    "Members":
-      [{
-        "Name": "Sashreek Krishnan",
-        "Reg_No": "20BRS1047"
+    Members: [
+      {
+        Name: "Sashreek Krishnan",
+        Reg_No: "20BRS1047",
       },
       {
-        "Name": "Satwika Sridhar",
-        "Reg_No": "20BCE1348"
+        Name: "Satwika Sridhar",
+        Reg_No: "20BCE1348",
       },
       {
-        "Name": "Satyam Singh",
-        "Reg_No": "20BEC1072"
+        Name: "Satyam Singh",
+        Reg_No: "20BEC1072",
       },
       {
-        "Name": "Sharen H",
-        "Reg_No": "20BAI1277"
+        Name: "Sharen H",
+        Reg_No: "20BAI1277",
       },
       {
-        "Name": "Shaun George Rajesh",
-        "Reg_No": "21BCE1295"
+        Name: "Shaun George Rajesh",
+        Reg_No: "21BCE1295",
       },
       {
-        "Name": "Shivansh Sharma",
-        "Reg_No": "21BCE1907"
+        Name: "Shivansh Sharma",
+        Reg_No: "21BCE1907",
       },
       {
-        "Name": "Shreya Tripathi",
-        "Reg_No": "21BCE1031"
+        Name: "Shreya Tripathi",
+        Reg_No: "21BCE1031",
       },
       {
-        "Name": "Shriyans A",
-        "Reg_No": "21BCE1121"
+        Name: "Shriyans A",
+        Reg_No: "21BCE1121",
       },
       {
-        "Name": "Siddharth Ramanathan",
-        "Reg_No": "20BEC1098"
+        Name: "Siddharth Ramanathan",
+        Reg_No: "20BEC1098",
       },
       {
-        "Name": "Smriti Vipin Madangarli",
-        "Reg_No": "21BCE1269"
+        Name: "Smriti Vipin Madangarli",
+        Reg_No: "21BCE1269",
       },
       {
-        "Name": "Sneha Sivaramakrishnan",
-        "Reg_No": "20BAI1251"
+        Name: "Sneha Sivaramakrishnan",
+        Reg_No: "20BAI1251",
       },
       {
-        "Name": "Somen Bandishti",
-        "Reg_No": "21BCE5063"
+        Name: "Somen Bandishti",
+        Reg_No: "21BCE5063",
       },
       {
-        "Name": "Sparsh Kandpal",
-        "Reg_No": "21BCE1961"
+        Name: "Sparsh Kandpal",
+        Reg_No: "21BCE1961",
       },
       {
-        "Name": "Sravan",
-        "Reg_No": "21BLC1018"
+        Name: "Sravan",
+        Reg_No: "21BLC1018",
       },
       {
-        "Name": "Srinitish Srinivasan",
-        "Reg_No": "21BAI1394"
-      }]
+        Name: "Srinitish Srinivasan",
+        Reg_No: "21BAI1394",
+      },
+    ],
   },
   {
-    "Members":
-      [{
-        "Name": "Sruthi Srinivasan",
-        "Reg_No": "19BAI1046"
+    Members: [
+      {
+        Name: "Sruthi Srinivasan",
+        Reg_No: "19BAI1046",
       },
       {
-        "Name": "Sujeeth Sundaram",
-        "Reg_No": "21BAI1084"
+        Name: "Sujeeth Sundaram",
+        Reg_No: "21BAI1084",
       },
       {
-        "Name": "Suriyakrishnan Sathish",
-        "Reg_No": "19BCE1050"
+        Name: "Suriyakrishnan Sathish",
+        Reg_No: "19BCE1050",
       },
       {
-        "Name": "Sushobith Girish",
-        "Reg_No": "20BPS1052"
+        Name: "Sushobith Girish",
+        Reg_No: "20BPS1052",
       },
       {
-        "Name": "Susindhar A V",
-        "Reg_No": "20BCE1973"
+        Name: "Susindhar A V",
+        Reg_No: "20BCE1973",
       },
       {
-        "Name": "Swati Laxmi Sahu",
-        "Reg_No": "19BCE1185"
+        Name: "Swati Laxmi Sahu",
+        Reg_No: "19BCE1185",
       },
       {
-        "Name": "Tavishi Rastogi",
-        "Reg_No": "21BCE1043"
+        Name: "Tavishi Rastogi",
+        Reg_No: "21BCE1043",
       },
       {
-        "Name": "Venkata Saaketha Krishna",
-        "Reg_No": "20BEC1253"
+        Name: "Venkata Saaketha Krishna",
+        Reg_No: "20BEC1253",
       },
       {
-        "Name": "Vikram Singh",
-        "Reg_No": "21BPS1615"
+        Name: "Vikram Singh",
+        Reg_No: "21BPS1615",
       },
       {
-        "Name": "Yadu Krishnan U",
-        "Reg_No": "20BAI1112"
+        Name: "Yadu Krishnan U",
+        Reg_No: "20BAI1112",
       },
       {
-        "Name": "Yadukrishnan",
-        "Reg_No": "21BAI1210"
+        Name: "Yadukrishnan",
+        Reg_No: "21BAI1210",
       },
       {
-        "Name": "Zahran Sajid",
-        "Reg_No": "21BPS1091"
-      }]
-  }
-]
+        Name: "Zahran Sajid",
+        Reg_No: "21BPS1091",
+      },
+    ],
+  },
+];
 
 export default MembersList;
