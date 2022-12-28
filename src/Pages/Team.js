a1265ac714bf0eca212b535845965af274b33229
Abhijith Ganesh <githubwatchabhijithganesh@gmail.com>
2022-12-28 18:37:01 +0530

    New heads (#27)
    * Addition of heads images
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    
    * Addition of new OBs
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>
    
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -3,14 +3,16 @@ import React, { useEffect, useState } from "react";
 import banner from "../images/banner.png";
 
 /* Profile Images */
-import gunja from "../images/gunja.jpg";
-import mainak from "../images/mainak.jpeg";
-import prathiba from "../images/prathiba.jpg";
-import samank from "../images/samank.jpeg";
-import sanika from "../images/sanika.jpg";
-import swapnal from "../images/Swapnal.jpeg";
-import tanay from "../images/tanay.jpeg";
-import tejas from "../images/tejas.jpeg";
+import prathiba from "../images/heads/prathiba.jpg";
+import ag from "../images/heads/ag.png";
+import dikshita from "../images/heads/dikshita.png";
+import harsh from "../images/heads/harsh.png";
+import anjali from "../images/heads/anjali-kedia.jpg";
+import santhanam from "../images/heads/santhanam.png";
+import neha from "../images/heads/neha.png";
+import sparsh from "../images/heads/sparsh.png";
+import saimirra from "../images/heads/saimirra.png";
+import aditya from "../images/heads/aditya.png";
 
 import MembersList from "../components/bookdata/MembersList";
 import OBList from "../components/bookdata/OBList";
@@ -19,15 +21,15 @@ import Mountains from "../components/Mountains";
 
 const TeamMember = ({ name, designation, profileLink }) => {
   return (
-    <div>
-      <div className="rounded-full overflow-hidden " id="metallic">
+    <section>
+      <section className="rounded-full overflow-hidden " id="metallic">
         <img src={profileLink} className="h-48" alt={name}></img>
-      </div>
-      <div className="rounded-md p-2 my-4 " id="designation-bg">
+      </section>
+      <section className="rounded-md p-2 my-4 " id="designation-bg">
         <h1 className="font-medium text-xl text-black">{name}</h1>
         <h2 className="font-normal text-black">{designation}</h2>
-      </div>
-    </div>
+      </section>
+    </section>
   );
 };
 
@@ -46,117 +48,131 @@ const Team = () => {
   }, []);
 
   return (
-    <div>
+    <section>
       {loading ? (
         <Loader />
       ) : (
-        <div className="relative z-10">
-          <div
+        <section className="relative z-10">
+          <section
             className="font-catamaran text-center my-10 text-black"
             id="outlinetext"
           >
             <h1>MEET THE TEAM</h1>
-          </div>
-          <div className="pt-1">
-            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-52">
-              <div className="grid justify-items-center lg:justify-items-end">
-                <div>
-                  <div className="rounded-full overflow-hidden " id="metallic">
-                    <img
-                      src={samank}
-                      className="h-48 lg:h-60"
-                      alt="Person"
-                    ></img>
-                  </div>
-                  <div className="rounded-md p-2 my-4 " id="designation-bg">
+          </section>
+          <section className="pt-1">
+            <section className="grid grid-cols-1 lg:grid-cols-2 lg:gap-52">
+              <section className="grid justify-items-center lg:justify-items-end">
+                <section>
+                  <section
+                    className="rounded-full overflow-hidden "
+                    id="metallic"
+                  >
+                    <img src={neha} className="h-48 lg:h-60" alt="Person" />
+                  </section>
+                  <section className="rounded-md p-2 my-4 " id="designation-bg">
                     <h1 className="font-medium text-xl text-black">
-                      Samank Gupta
+                      Neha Ragesh Nambiar
                     </h1>
                     <h2 className="font-normal text-black">President</h2>
-                  </div>
-                </div>
-              </div>
-              <div className="grid justify-items-center lg:justify-items-start">
-                <div>
-                  <div className="rounded-full overflow-hidden " id="metallic">
-                    <img
-                      src={tanay}
-                      className="h-48 lg:h-60"
-                      alt="Person"
-                    ></img>
-                  </div>
-                  <div className="rounded-md p-2 my-4 " id="designation-bg">
+                  </section>
+                </section>
+              </section>
+              <section className="grid justify-items-center lg:justify-items-start">
+                <section>
+                  <section
+                    className="rounded-full overflow-hidden "
+                    id="metallic"
+                  >
+                    <img src={harsh} className="h-48 lg:h-60" alt="Person" />
+                  </section>
+                  <section className="rounded-md p-2 my-4 " id="designation-bg">
                     <h1 className="font-medium text-xl text-black">
-                      Tanay Bhadula
+                      Harsh Aryan
                     </h1>
                     <h2 className="font-normal text-black">Vice President</h2>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="grid lg:grid-cols-3 lg:gap-20 lg:my-24 grid-cols-1">
-              <div className="grid  justify-items-center lg:justify-items-end">
+                  </section>
+                </section>
+              </section>
+            </section>
+            <section className="grid lg:grid-cols-3 lg:gap-20 lg:my-24 grid-cols-1">
+              <section className="grid  justify-items-center lg:justify-items-end">
                 <TeamMember
-                  name="Sanika Kulkarni"
+                  name="Dikshita Mehta"
                   designation="General Secretary"
-                  profileLink={sanika}
+                  profileLink={dikshita}
                 />
-              </div>
-              <div className="grid justify-items-center">
+              </section>
+              <section className="grid justify-items-center">
                 <TeamMember
-                  name="Mainak Sengupta"
+                  name="Abhijith Ganesh"
                   designation="Technical Head"
-                  profileLink={mainak}
+                  profileLink={ag}
                 />
-              </div>
-              <div className="grid justify-items-center lg:justify-items-start">
+              </section>
+              <section className="grid justify-items-center lg:justify-items-start">
                 <TeamMember
-                  name="Swapnal Varma"
+                  name="Anjali Kedia"
                   designation="Joint General Secretary"
-                  profileLink={swapnal}
+                  profileLink={anjali}
                 />
-              </div>
-            </div>
-            <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-20 lg:mb-24">
-              <div className="grid justify-items-center lg:justify-items-end ">
+              </section>
+            </section>
+            <section className="grid grid-cols-1 lg:grid-cols-3 lg:gap-20 lg:mb-24">
+              <section className="grid justify-items-center lg:justify-items-end ">
                 <TeamMember
-                  name="Tejas Vaichole"
+                  name="Aditya Naryan Mahapatra"
                   designation="Design Head"
-                  profileLink={tejas}
+                  profileLink={aditya}
                 />
-              </div>
-              <div className="grid justify-items-center">
+              </section>
+              <section className="grid justify-items-center">
                 <TeamMember
                   name="Gunja Pandey"
                   designation="Public Relations head"
-                  profileLink={gunja}
+                  profileLink={saimirra}
                 />
-              </div>
-              <div className="grid justify-items-center lg:justify-items-start ">
+              </section>
+              <section className="grid justify-items-center lg:justify-items-start ">
                 <TeamMember
                   name="Prathiba Narayan"
                   designation="Design Head"
                   profileLink={prathiba}
                 />
-              </div>
-            </div>
-          </div>
-          <div className="text-center mt-12 lg:mt-0">
+              </section>
+            </section>
+            <section className="grid grid-cols-1 lg:grid-cols-3 lg:gap-20 lg:mb-24">
+              <section className="grid justify-items-center lg:justify-items-end ">
+                <TeamMember
+                  name="B Santhanakrishanan"
+                  designation="Projects Head"
+                  profileLink={santhanam}
+                />
+              </section>
+              <section className="grid justify-items-center lg:justify-items-start lg:col-start-3 ">
+                <TeamMember
+                  name="Sparsh Kandpal"
+                  designation="Treasurer"
+                  profileLink={sparsh}
+                />
+              </section>
+            </section>
+          </section>
+          <section className="text-center mt-12 lg:mt-0">
             <button
               onClick={() => setModalDisplay(true)}
               className="bg-gradient-to-r from-color1 to-color2 inline-flex justify-center py-2 px-10 border lg:border-2 border-transparent shadow-xl hover:shadow-sm text-xl font-medium mr-auto ml-auto rounded-full text-white hover:from-comsocgreen"
             >
               Members List
             </button>
-          </div>
-          <div
+          </section>
+          <section
             className={
               "h-screen w-screen bg-transparent z-30 absolute" +
               (modalDisplay ? " block" : " hidden")
             }
             id="memberslist"
           >
-            <div className="relative z-40">
+            <section className="relative z-40">
               <button
                 onClick={() => setModalDisplay(false)}
                 className="fixed top-9 right-2 lg:right-52"
@@ -176,50 +192,50 @@ const Team = () => {
                   />
                 </svg>
               </button>
-            </div>
-            <div
+            </section>
+            <section
               className="h-full w-full bg-black bg-opacity-80 fixed top-0 overflow-y-scroll"
               id="memberCarousel"
             >
-              <div className="mx-10 lg:mx-72 my-10 relative z-30">
-                <div className="bg-gray-900 my-2 lg:my-4">
+              <section className="mx-10 lg:mx-72 my-10 relative z-30">
+                <section className="bg-gray-900 my-2 lg:my-4">
                   <img src={banner} alt="Banner" />
-                </div>
+                </section>
                 {OBList.map((ob) => (
-                  <div className="bg-gray-900 border lg:border-2 border-comsocgreen rounded-xl my-2 lg:my-4 py-4">
+                  <section className="bg-gray-900 border lg:border-2 border-comsocgreen rounded-xl my-2 lg:my-4 py-4">
                     <h1 className="text-base lg:text-2xl font-semibold text-comsocgreen mb-4">
                       OB {ob.Year}
                     </h1>
-                    <div className="grid grid-cols-2">
+                    <section className="grid grid-cols-2">
                       {ob.Members.map((mem) => (
                         <p className="text-xs lg:text-lg p-1">
                           {mem.Name} - {mem.Designation}
                         </p>
                       ))}
-                    </div>
-                  </div>
+                    </section>
+                  </section>
                 ))}
                 {MembersList.map((member) => (
-                  <div className="bg-gray-900 border lg:border-2 border-comsocgreen rounded-xl my-2 lg:my-4 py-4">
+                  <section className="bg-gray-900 border lg:border-2 border-comsocgreen rounded-xl my-2 lg:my-4 py-4">
                     <h1 className="text-base lg:text-2xl font-semibold text-comsocgreen mb-4">
                       Members 2021 - 2022
                     </h1>
-                    <div className="grid grid-cols-3">
+                    <section className="grid grid-cols-3">
                       {member.Members.map((mem) => (
                         <p className="text-xs lg:text-lg p-1">{mem.Name}</p>
                       ))}
-                    </div>
-                  </div>
+                    </section>
+                  </section>
                 ))}
-                <div className="bg-gray-900 my-2 lg:my-4" />
-                <div className="bg-gray-900 my-2 lg:my-4" />
-              </div>
-            </div>
-          </div>
+                <section className="bg-gray-900 my-2 lg:my-4" />
+                <section className="bg-gray-900 my-2 lg:my-4" />
+              </section>
+            </section>
+          </section>
           <Mountains />
-        </div>
+        </section>
       )}
-    </div>
+    </section>
   );
 };
 
